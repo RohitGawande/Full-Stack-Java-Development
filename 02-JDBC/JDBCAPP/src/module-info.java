5a7dbcffaf4f04414a53b0601043ff704b31d714
Rohit Gawande <rohitgawande2004@gmail.com>
2025-08-21 17:57:17 +0530

    JDBC(Part 2)

---
+++ 02-JDBC/JDBCAPP/src/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * @author Asus
+ *
+ */
+module JDBCAPP {
+	requires java.sql;
+}
---
+++ 02-JDBC/JDBCStandardApp/src/in/rohit/main/UpdateApp.java
@@ -0,0 +1,49 @@
+package in.rohit.main;
+
+import java.sql.*;
+
+public class UpdateApp {
+
+    public static void main(String[] args) {
+
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultset = null;
+        try {
+            
+
+            // Step 2: Establish the Connection
+            String url = "jdbc:mysql:///Rohit";
+            String user = "Rohit";
+            String password = "Rohit2004";
+            connection = DriverManager.getConnection(url, user, password);
+            System.out.println("Connection object created...");
+
+            // Step 3: Create statement Object and send the Query
+        
+            statement = connection.createStatement();
+            System.out.println("Statement object created...");
+
+            //Step 4: Execute the Query and Process the resultset 
+            String sqlUpdateQuery = "update student set name='Rohit Sharma' where id=5"; 
+            int rowAffected = statement.executeUpdate(sqlUpdateQuery);
+            System.out.println("No of Rows affected is::"+rowAffected);
+          
+            
+        
+        } catch (SQLException se) {
+            se.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // Step 6: Closing the resources 
+            try {
+                if (resultset != null) resultset.close();
+                if (statement != null) statement.close();
+                if (connection != null) connection.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+    }
+}
