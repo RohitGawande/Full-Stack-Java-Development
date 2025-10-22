c9db7c3cd28f488d1ac95ef1545052ee8cd97731
Rohit Gawande <rohitgawande2004@gmail.com>
2025-10-22 11:05:14 +0530

    Spring Core

--- Spring Core/04-ConstructorSetterInjection/src/in/rohit/test/ConstructorInjectionUsingApplicationContext.java
+++ Spring Core/04-ConstructorSetterInjection/src/in/rohit/test/ConstructorInjectionUsingApplicationContext.java
@@ -1,42 +1,27 @@
-package in.rohit.test;
+package in.ineuron.test;
 
-import java.util.Arrays;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import in.ineuron.comp.Student;
 
-import in.rohit.comp.WishMessageGenerator;
-
-public class ConstructorInjectionUsingApplicationContext {
+public class AdvancedBeanFactoryApp {
 
 	public static void main(String[] args) throws Exception {
 
-		System.out.println("***********ApplicationContext container starting************");
-		//Eager Loading happens
-		ClassPathXmlApplicationContext factory = new ClassPathXmlApplicationContext(
-				"applicationContext.xml");
+		DefaultListableBeanFactory factory = null;
+		XmlBeanDefinitionReader reader = null;
 
-		System.out.println("No of beans is :: " + factory.getBeanDefinitionCount());
-		System.out.println("Bean id  is :: " + Arrays.toString(factory.getBeanDefinitionNames()));
-
-		System.out.println("\n***********ApplicationContext container started*************");
-
+		factory = new DefaultListableBeanFactory();
+		reader = new XmlBeanDefinitionReader(factory);
+		reader.loadBeanDefinitions("in/ineuron/cfg/applicationContext.xml");
+		System.out.println("****Container started*****");
+		
 		System.in.read();
 
-		WishMessageGenerator wmg = factory.getBean("wmg", WishMessageGenerator.class);
-		System.out.println("Hashcode value of wmg is :: " + wmg.hashCode());
-		System.out.println(wmg);
-		String result = wmg.generateMessage("sachin");
-		System.out.println(result + "\n");
+		Student student = factory.getBean("std1", Student.class);
+		System.out.println(student); 
+		System.out.println("\n****Container closed*****");
 
-		WishMessageGenerator wmg1 = factory.getBean("wmg", WishMessageGenerator.class);
-		System.out.println("HashCode value of wmg1 is :: " + wmg1.hashCode());
-		System.out.println(wmg1);
-		String result1 = wmg1.generateMessage("kohli");
-		System.out.println(result1);
-
-		System.out.println();
-
-		System.out.println("*******Container is closing**********");
-		factory.close();
 	}
 }
