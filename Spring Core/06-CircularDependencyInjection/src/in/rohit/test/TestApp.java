b4938219c2876458efd190e656fd0476562458a8
Rohit Gawande <rohitgawande2004@gmail.com>
2025-11-03 10:42:44 +0530

    Circular Dependency Injection

--- Spring Core/06-CircularDependencyInjection/src/in/rohit/test/TestApp.java
+++ Spring Core/06-CircularDependencyInjection/src/in/rohit/test/TestApp.java
@@ -1,9 +1,10 @@
-package in.rohit.test;
+package in.ineuron.test;
 
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 
-import in.rohit.comp.Flipkart;
+import in.ineuron.comp.A;
+import in.ineuron.comp.B;
 
 public class TestApp {
 
@@ -11,16 +12,14 @@ public class TestApp {
 
 		DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
 		XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(factory);
-		reader.loadBeanDefinitions("in/rohit/cfg/applicationContext.xml");
+		reader.loadBeanDefinitions("in/ineuron/cfg/applicationContext.xml");
 		System.out.println("*****CONTAINER STARTED****\n");
 
 		System.in.read();
 
-		Flipkart flipkart = factory.getBean("fpkt", Flipkart.class);
-		System.out.println(flipkart);
-		System.out.println();
-		String result = flipkart.doShopping(new String[] { "fossil", "tissot" }, new float[] { 23456.5f, 12345.5f });
-		System.out.println(result);
+		A aObj = factory.getBean("a1", A.class);
+		
+		B bObj = factory.getBean("b1", B.class);
 
 		System.out.println("\n*****CONTAINER STOPPED****");
 
