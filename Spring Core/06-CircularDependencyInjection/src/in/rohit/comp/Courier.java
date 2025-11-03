95ab151257748592974891fc7094f3606de04e45
Rohit Gawande <rohitgawande2004@gmail.com>
2025-11-03 10:41:31 +0530

    Circular Dependency Injection

--- Spring Core/06-CircularDependencyInjection/src/in/rohit/comp/Courier.java
+++ Spring Core/06-CircularDependencyInjection/src/in/rohit/comp/Courier.java
@@ -1,5 +1,30 @@
-package in.rohit.comp;
+package in.ineuron.comp;
 
-public interface Courier {
-	public String deliver(int oid);
+public class B {
+
+	private A a;
+
+	static {
+		System.out.println("B.class file is loading...");
+	}
+
+	public B() {
+		System.out.println("B: zero param constructor");
+	}
+
+	public B(A a) {
+		this.a = a;
+		System.out.println("B.B():: 1 Param constructor...");
+	}
+
+	public void setA(A a) {
+		this.a = a;
+		System.out.println("B.setA()");
+	}
+
+	@Override
+	public String toString() {
+		return "B [a=" + a + "]";
+	}
+
 }
