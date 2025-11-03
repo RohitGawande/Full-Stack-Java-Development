de9f316d661664ef50bbddb0f5d7cdda678f76cf
Rohit Gawande <rohitgawande2004@gmail.com>
2025-11-03 10:42:58 +0530

    Circular Dependency Injection

--- Spring Core/06-CircularDependencyInjection/src/in/rohit/test/TestApp.java
+++ Spring Core/06-CircularDependencyInjection/src/in/rohit/test/TestApp.java
@@ -1,4 +1,4 @@
-package in.ineuron.test;
+package in.rohit.test;
 
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
